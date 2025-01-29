062979c46f8c828bc4985da94eecd0b8fc0fd362
Tytus Felbor <146979682+TFelbor@users.noreply.github.com>
2025-01-29 10:30:18 -0500

    Update SkipCard.java

--- SkipCard.java
+++ SkipCard.java
@@ -5,6 +5,7 @@ package unoPackage;
 public abstract class SkipCard extends Card {
 
 	protected void Skipcard(int color) {
+		
 		this.color = color;
 		this.value = Card.skip;
 	}
@@ -12,12 +13,13 @@ public abstract class SkipCard extends Card {
 	protected boolean check(Table t){
 		
 		if (t.onTop.color == this.color) {
+			
 			return true;
+			
 		}
 		else {
-			return false;
-		}
 			
-	}
-	
+			return false;
+		}		
+	}	
 }
