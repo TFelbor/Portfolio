76d9423098d43c658b3d2991eed9afc3ca4e4286
Tytus Felbor <146979682+TFelbor@users.noreply.github.com>
2025-01-30 10:55:37 -0500

    Update ManySorts.java

--- ManySorts.java
+++ ManySorts.java
@@ -227,4 +227,23 @@ public class ManySorts {
 			stoogesort(arr, l, h - t);
 		}
 	}
+	
+	public static void reverseBubbleSort(int[] arr) {
+		int n = arr.length;
+
+		for (int i = 0; i < n - 1; i++) {
+			
+			for (int j = 0; j < n - i - 1; j++) {
+				
+				if (arr[j] < arr[j + 1]) {
+					
+					// Swap elements if they are in the wrong order
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+	
 }
